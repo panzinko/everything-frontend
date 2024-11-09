ef07f65020ffc1d802e3d6a08ead0f1564a332dc
Adrian Zinko <zinko.adrian00@gmail.com>
2024-11-09 18:09:07 +0100

    feat: add integration with open weather

---
+++ lib/openweather-api-client/models/index.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Alert';
+export * from './CurrentWeather';
+export * from './DailyForecast';
+export * from './DailyForecastFeelsLike';
+export * from './DailyForecastTemp';
+export * from './ErrorResponse';
+export * from './HourlyForecast';
+export * from './OneCallResponse';
+export * from './OnecallClimateGet200Response';
+export * from './WeatherInfo';
